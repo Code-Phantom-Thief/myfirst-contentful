20eb9ddb79658926f690252104ec99befef63dbd
Code-Phantom-Thief <code.phantom.thief@gmail.com>
2021-10-15 08:24:45 +0900

    notfound

--- pages/404.js
+++ pages/404.js
@@ -14,7 +14,7 @@ const NotFound = () => {
 	return (
 		<div className='not-found'>
 			<h1>404</h1>
-			<h2>Ooops! That page cannot be dound: (</h2>
+			<h2>Ooops! That page cannot be found: (</h2>
 			<p>
 				Redirecting to{' '}
 				<Link href='/'>
